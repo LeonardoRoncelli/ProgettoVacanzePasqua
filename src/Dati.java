8ed02c420785a941f3afa697ae1cb42b20a8d09d
Roncelli <leonardoroncelli@gmail.com>
2025-04-18 10:22:41 +0200

    Classe Dati.

--- src/Dati.java
+++ src/Dati.java
@@ -4,17 +4,29 @@ public class Dati {
     public String getTema() {
         return tema;
     }
-    public void setTema(String tema) {
-        this.tema = tema;
+    public void setTema(String tema) throws Exception {
+        if(tema.equalsIgnoreCase("Argomento1")||tema.equalsIgnoreCase("Argomento2")||tema.equalsIgnoreCase("Argomento3")) {
+            this.tema = tema;
+        }else {
+            throw new Exception("Tema non disponibile");
+        }
     }
     public int getVoto() {
         return voto;
     }
-    public void setVoto(int voto) {
-        this.voto = voto;
+    public void setVoto(int voto) throws Exception {
+        if(voto>=1&&voto<=10) {
+            this.voto = voto;
+        }else{
+            throw new Exception("Voto errato");
+        }
     }
-    public Dati(int voto,String tema){
-        this.voto=voto;
-        this.tema=tema;
+    public Dati(int voto,String tema) throws Exception {
+        setVoto(voto);
+        setTema(tema);
+    }
+    public String toString(){
+        return "Voto: "+voto+
+                "\nTema: "+tema;
     }
 }
